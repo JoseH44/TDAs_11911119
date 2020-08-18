3a8d6621e685e6d8ce408995aa510bad1ae74344
Jose <jrdjhc45@gmail.com>
2020-08-17 23:42:34 -0600

    Implementación de metodos de la clase ArrayStack

--- ArrayStack.cpp
+++ ArrayStack.cpp
@@ -8,10 +8,9 @@ ArrayStack::ArrayStack()
 }
 
 void ArrayStack::push(Object* x){
-	//asignar x a la casilla tope
-	//incrementar tope
-	array[tope] = x;
-	tope++;
+
+	array[tope] = x;//asigna x a la casilla tope
+	tope++;//incrementa tope
 }
 
 bool ArrayStack::isEmpty(){
@@ -19,31 +18,48 @@ bool ArrayStack::isEmpty(){
 }
 
 Object* ArrayStack::pop(){
-	//si no est� vac�a 
-		//recuperar el valor de la casilla tope-1 
-		//asignar null a tope-1
-		//tope--
-		//retornar el valor recuperado
-	//else return null
+	//si no está vacía 
+	if (!isEmpty())
+	{
+		Object* retValue; 
+		retValue =array[tope-1];//recuperar el valor de la casilla tope-1 
+		array[tope-1] = NULL;//asigna null a tope-1
+		tope--;//se decrementa tope(tope--)
+		return retValue;//retorna el valor recuperado
+	}else
+	{
+		return NULL;
+	}
+
 }
 
 void ArrayStack::print(){
 	//for i = tope-1 hasta 1
-		//imprimir el contenido de la casilla i del array
+	for (int i = tope-1; i >= 1; i--)
+	{
+		cout<<array[i];//imprime el contenido de la casilla i del array
+	}
 }
 
 Object* ArrayStack::top() {
-	//si no est� vac�a 
-		//recuperar el valor de la casilla tope-1 
-		//retornar el valor recuperado
-	//else return null
+	//si no está vacía
+	if (!isEmpty())
+	{
+		Object* retValue;//variable que guardará el dato 
+		retValue = array[tope-1];//recupera el valor de la casilla tope-1 
+		return retValue;//retorna el valor recuperado
+	}else
+	{
+		return NULL;
+	}
+	
 }
 
 void ArrayStack::clear(){
 	for(int i = 0 ; i < tope;i++ )
 		delete array[i];
 		
-	tope =0;
+	tope = 0;
 }
 
 ArrayStack::~ArrayStack()
